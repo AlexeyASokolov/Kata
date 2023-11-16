25c2577aa712a1a045ec95eaf5c6560cd720ae64
AlexeyASokolov <vdobriyputyout@gmail.com>
2023-11-16 14:03:40 +0300

    kl;

--- src/ArrayList/Test/ArrrTest.java
+++ src/ArrayList/Test/ArrrTest.java
@@ -4,21 +4,21 @@ import java.util.Arrays;
 
 public class ArrrTest {
     public static void main(String[] args) {
-        int[] array1 = {76543,6543};
+        int[] array1 = {1, 5, 2, 17};
         int[] array2 = {543};
-        int[] array3 = {};
-        int[] array4= {876543,44,443,76,876,876,3456};
-        int[] array5 = {543,4,3,6,76,346};
-        System.out.println(Arrays.toString(getSubArrayBetween(array1)));
-        System.out.println(Arrays.toString(getSubArrayBetween(array2)));
-        System.out.println(Arrays.toString(getSubArrayBetween(array3)));
-        System.out.println(Arrays.toString(getSubArrayBetween(array4)));
-        System.out.println(Arrays.toString(getSubArrayBetween(array5)));
+        int[] array3 = {14, 16, 3};
+        int[] array4 = {876543, 44, 443, 76, 876, 876, 3456};
+        int[] array5 = {543, 4, 3, 6, 76, 346};
+        System.out.println(Arrays.toString(getArrayMiddle(array1)));
+        System.out.println(Arrays.toString(getArrayMiddle(array2)));
+        System.out.println(Arrays.toString(getArrayMiddle(array3)));
+        System.out.println(Arrays.toString(getArrayMiddle(array4)));
+        System.out.println(Arrays.toString(getArrayMiddle(array5)));
 
     }
 
-    public static int[] getSubArrayBetween(int[] numbers) {
-        //твой код тут
+    public static int[] getArrayMiddle(int[] numbers) {
+//твой код тут
         if (numbers.length == 0) {
             return new int[0];
         } else if (numbers.length == 1) {
@@ -29,9 +29,7 @@ public class ArrrTest {
 
             int length = 0;
             for (int i = 0; i < numbers.length / 2; i++) {
-                if (i < numbers.length / 2) {
-                    length++;
-                }
+                length++;
             }
             int[] array = new int[length];
             int index = 0;
@@ -46,11 +44,14 @@ public class ArrrTest {
             int index2 = length - 1;
 
             for (int i = numbers.length - 1; i > numbers.length / 2 - 1; i--) {
-                array2[index2] = numbers[i];
-                index2--;
+                if (index2 >= 0) {
+                    array2[index2] = numbers[i];
+                    index2--;
+                }
+
             }
             int[] arrayResult1 = new int[2];
-            arrayResult1[0] = array[2];
+            arrayResult1[0] = array[1];
             arrayResult1[1] = array2[0];
 
             return arrayResult1;
